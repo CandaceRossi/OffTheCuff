c74f288d71e58d570e2b8dfc263d3a7e107de2b0
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2022-03-10 09:44:42 -0500

    backend set up ready for seeding

--- BE/data/migrations/20220117032515_comments.js
+++ BE/data/migrations/20220117032515_comments.js
@@ -1,25 +1,25 @@
 exports.up = function(knex) {
-return knex.schema.createTable("comments", col => {
-    col.increments("comments_id")
-    .notNullable()
-    .unique()
-    col.varchar('name', 255)
-      .notNullable()
-    col.text('description', 255)
-      .notNullable();
-    col
-      .integer("posts_id")
-      .unsigned()
-      .notNullable()
-      .references("posts_id")
-      .inTable("posts")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
-    col.timestamp('create_at').defaultTo(knex.fn.now())
-  })
+    return knex.schema.createTable("comments", col => {
+        col.increments("comments_id")
+            .notNullable()
+            .unique()
+        col.varchar('name', 255)
+            .notNullable()
+        col.text('description', 255)
+            .notNullable();
+        col
+            .integer("posts_id")
+            .unsigned()
+            .notNullable()
+            .references("posts_id")
+            .inTable("posts")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        col.timestamp('create_at').defaultTo(knex.fn.now())
+    })
 
 };
 
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("comments");
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("comments");
 };
